a25c9a7b7e1eb297b20a4c2affa6472c5c890859
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-06-24 23:34:04 +0900

    feat(files): pass current buffer name to mini.files.open

--- nvim/plugins/files.lua
+++ nvim/plugins/files.lua
@@ -1,12 +1,12 @@
 return {
-	name = "mini.files",
-	repo = "echasnovski/mini.files",
-	lua_source = function()
-		require("mini.files").setup()
-	end,
-	lua_add = function()
-		vim.keymap.set("n", "<Leader>e", function()
-			require("mini.files").open()
-		end)
-	end,
+    name = "mini.files",
+    repo = "echasnovski/mini.files",
+    lua_source = function()
+        require("mini.files").setup()
+    end,
+    lua_add = function()
+        vim.keymap.set("n", "<Leader>e", function()
+            require("mini.files").open(vim.api.nvim_buf_get_name(0))
+        end)
+    end,
 }
