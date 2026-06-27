bcc5af5310bdaafeeb5ea000222fb2013125607e
airRnot1106 <62370527+airRnot1106@users.noreply.github.com>
2026-06-27 23:46:19 +0900

    chore(nvim): enable lua loader and disable unused default plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,39 @@
+-- enable the lua module cache before requiring anything else
+vim.loader.enable()
+
+-- disable unused default runtime plugins
+-- tohtml
+vim.g.loaded_2html_plugin = true
+
+-- archive file open and browse
+vim.g.loaded_gzip = true
+vim.g.loaded_tar = true
+vim.g.loaded_tarPlugin = true
+vim.g.loaded_zip = true
+vim.g.loaded_zipPlugin = true
+
+-- vimball
+vim.g.loaded_vimball = true
+vim.g.loaded_vimballPlugin = true
+
+-- netrw (mini.files is used instead; gx is native since 0.10)
+vim.g.loaded_netrw = true
+vim.g.loaded_netrwPlugin = true
+vim.g.loaded_netrwSettings = true
+vim.g.loaded_netrwFileHandlers = true
+
+-- getlatestvimscript
+vim.g.loaded_getscript = true
+vim.g.loaded_getscriptPlugin = true
+
+-- misc
+vim.g.loaded_spellfile_plugin = true
+vim.g.loaded_tutor_mode_plugin = true
+vim.g.did_install_default_menus = true
+vim.g.did_install_syntax_menu = true
+vim.g.skip_loading_mswin = true
+vim.g.loaded_rrhelper = true
+
 -- Must be set before load_state() so startup.vim hooks expand <Leader> correctly
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
